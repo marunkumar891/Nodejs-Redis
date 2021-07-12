6bf550d0ae6dfc977b7f5908a73432c42dfccd73
ArunKumar <arun.i@xpresslane.in>
2021-07-12 16:20:55 +0530

    Code cleanup

--- services/repoService.js
+++ services/repoService.js
@@ -4,14 +4,13 @@ function setResponse(username, repos) {
     return `<h2>${username} has ${repos} Github repos</h2>`;
 }
 
-exports.GetRepoDetails = async function (req, res, next, username) {
+exports.GetRepoDetails = async (username) => {
 
     console.log('Getting Repo Details');
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
     const repos = data.public_repos;
-    console.log(response)
 
-    return (response)
+    return (repos)
 
 }
